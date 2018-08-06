3560d6afbaacc18141519bea0b0db952a32b768b
ryosei.kojima <ryosei.8721.soccer@icloud.com>
2018-08-06 05:35:21 +0000

    DRY

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -28,12 +28,30 @@ class MessagesController < ApplicationController
   end
   
   def update
+    @message = Message.find(params[:id])
+
+    if @message.update(message_params)
+      flash[:success] = 'Message は正常に更新されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'Message は更新されませんでした'
+      render :edit
+    end
   end
   
   def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+
+    flash[:success] = 'Message は正常に削除されました'
+    redirect_to messages_url
   end
   
   private
+  
+  def set_message
+    @message = Message.find(params[:id])
+  end
 
   # Strong Parameter
   def message_params
